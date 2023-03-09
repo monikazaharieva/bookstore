8431c673d268aa4b0f3f6a9e40915d6d50712d73
Cwetan <cvetan.atanasov@abv.bg>
2023-03-09 18:34:48 +0200

    init

---
+++ BookStoreB/BookStore.BL/Interfaces/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models.Data;
+
+namespace BookStore.BL.Interfaces
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+
+        Book GetById(int id);
+
+        void AddBook(Book book);
+
+        void DeleteBook(int id);
+    }
+}
---
+++ BookStoreB/BookStoreB/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("GetAll")]
+        public IEnumerable<Book> GetAll()
+        {
+            return _bookService.GetAll();
+        }
+
+        [HttpGet("GetById")]
+        public Book GetById(int id)
+        {
+            return _bookService.GetById(id);
+        }
+
+        [HttpPost("Add")]
+        public void Add([FromBody] Book book)
+        {
+            _bookService.AddBook(book);
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(int bookId)
+        {
+            _bookService.DeleteBook(bookId);
+        }
+    }
+}
